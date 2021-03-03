c015c1067653aa25408bd99101eb8f1683cee139
drjiky <78235966+drjiky@users.noreply.github.com>
2021-03-03 22:10:10 +0100

    Create lab11.py

---
+++ module3/lab11.py
@@ -0,0 +1,31 @@
+# step 1
+beatles=[]
+print("Step 1:", beatles)
+
+# step 2
+beatles.append("John Lennon")
+beatles.append("Paul McCartney")
+beatles.append("George Harrison")
+
+print("Step 2:", beatles)
+
+# step 3
+for i in range(2):
+    x=input()
+    beatles.append(x)
+
+print("Step 3:", beatles)
+
+# step 4
+del beatles[4]
+del beatles[3]
+
+print("Step 4:", beatles)
+
+# step 5
+beatles.insert(0,"ringo starr")
+print("Step 5:", beatles)
+
+
+# testing list legth
+print("The Fab", len(beatles))
